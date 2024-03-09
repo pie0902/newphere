06a7b1dedbe864140a009a0ef1006d692f218061
pie0902 <zhfoqkq@gmail.com>
2024-03-09 15:40:09 +0900

    fetch

--- src/main/java/news/newsphere/controller/PostController.java
+++ src/main/java/news/newsphere/controller/PostController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import news.newsphere.dto.PostRequest;
 import news.newsphere.dto.PostResponse;
+import news.newsphere.entity.User;
 import news.newsphere.service.PostService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class PostController {
     private final PostService postService;
+    //게시글 생성
     @PostMapping("/posts")
     public ResponseEntity<PostResponse> createPost(
         @RequestBody PostRequest postRequest,
@@ -28,10 +33,12 @@ public class PostController {
         PostResponse postResponse = postService.createPost(postRequest,cleanedToken);
         return ResponseEntity.ok().body(postResponse);
     }
+    //전체조회
     @GetMapping("/posts")
     public ResponseEntity<List<PostResponse>> getAllPost()
     {
         List<PostResponse> postList = postService.getAllPost();
         return ResponseEntity.ok(postList);
     }
+
 }
--- src/main/java/news/newsphere/repository/PostRepository.java
+++ src/main/java/news/newsphere/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package news.newsphere.repository;
 
 
+import java.util.Optional;
 import news.newsphere.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.expression.spel.ast.OpAnd;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
